9c300bd7da20d1826fb5776a3e5742ed8a3343a6
unknown <vishal.rathore7@gmail.com>
2018-08-05 23:43:52 +0530

    Initial commit

--- src/test/java/com/testng/AppTest.java
+++ src/test/java/com/testng/AppTest.java
@@ -1,154 +1,148 @@
 package com.testng;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 public class AppTest {
 
-	ChromeOptions options;
-	WebDriver webdriver;
-	File screenshotFile;
-	String screenShotName = "test";
+    private ChromeOptions options;
+    private WebDriver webdriver;
 
-	DateFormat dateformate = new SimpleDateFormat("dd-mm-yy-hh-mm-ss");
+    /**
+     * Setting the chrome driver.exe path
+     *
+     * @param filepath chromedriver.exe path
+     */
+    @BeforeClass
+    @Parameters({"chromeExeFilePath"})
+    public void setChromeExeFilePath(String filepath) {
+        options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        System.setProperty("webdriver.chrome.driver", filepath);
 
-	/**
-	 * Setting the chrome driver.exe path
-	 * 
-	 * @param filepath
-	 * @throws IOException
-	 */
-	@BeforeClass
-	@Parameters({ "chromeExeFilePath" })
-	public void setChromeExeFilePath(String filepath) throws IOException {
-		options = new ChromeOptions();
-		options.addArguments("--disable-notifications");
-		System.setProperty("webdriver.chrome.driver", filepath);
+    }
 
-	}
+    /**
+     * open chrome browser and navigate to facebook.com
+     */
+    @Test
+    public void openBrowser() throws IOException {
+        webdriver = new ChromeDriver(options);
+        webdriver.manage().window().maximize();
+        webdriver.get("http://facebook.com");
 
-	/**
-	 * open chrome browser and navigate to facebook.com
-	 * 
-	 * @throws IOException
-	 */
-	@Test(priority = 0)
-	public void openBrowser() throws IOException {
-		webdriver = new ChromeDriver(options);
-		webdriver.manage().window().maximize();
-		webdriver.get("http://facebook.com");
+        // to take screenshot
+        ScreenShotUtil.takeScreenShot(webdriver);
+    }
 
-		webdriver = ScreenShotUtil.takeScreenShot(webdriver);
-	}
+    /**
+     * Make sure to update your facebook username and password in testng.xml file
+     * This method is used to login with username and password
+     *
+     * @param user facebook username
+     * @param pass facebook password
+     */
+    @Parameters({"username", "password"})
+    @Test(priority = 1)
+    public void login(String user, String pass) throws IOException {
+        WebElement username = webdriver.findElement(By.id("email"));
+        username.clear();
+        username.sendKeys(user);
 
-	/**
-	 * Make sure to update your facebook username and password in testng.xml file
-	 * This method is used to login with username and password
-	 * 
-	 * @param user
-	 * @param pass
-	 * @throws InterruptedException
-	 * @throws IOException
-	 */
-	@Parameters({ "username", "password" })
-	@Test(priority = 1)
-	public void login(String user, String pass) throws InterruptedException, IOException {
-		WebElement username = webdriver.findElement(By.id("email"));
-		username.clear();
-		username.sendKeys(user);
+        WebElement password = webdriver.findElement(By.id("pass"));
+        password.clear();
+        password.sendKeys(pass);
+        // to take screenshot
+        ScreenShotUtil.takeScreenShot(webdriver);
+        password.sendKeys(Keys.ENTER);
+    }
 
-		WebElement password = webdriver.findElement(By.id("pass"));
-		password.clear();
-		password.sendKeys(pass);
-		password.sendKeys(Keys.ENTER);
+    /**
+     * click on facebook profile
+     */
+    @Test(priority = 2)
+    public void testClickProfile() throws IOException {
+        WebElement profile = webdriver.findElement(By.className("_1qv9"));
+        profile.click();
 
-		webdriver = ScreenShotUtil.takeScreenShot(webdriver);
+        // to take screenshot
+        ScreenShotUtil.takeScreenShot(webdriver);
 
-	}
+    }
 
-	@Test(priority = 2)
-	public void testClickProfile() throws IOException {
-		WebElement profile = webdriver.findElement(By.className("_1qv9"));
-		profile.click();
+    /**
+     * click on facebook home
+     */
+    @Test(priority = 3)
+    public void testClickHome() throws IOException {
+        WebElement profile = webdriver.findElement(By.id("u_0_c"));
+        profile.click();
 
-		webdriver = ScreenShotUtil.takeScreenShot(webdriver);
+        // to take screenshot
+        ScreenShotUtil.takeScreenShot(webdriver);
 
-	}
+    }
 
-	@Test(priority = 3)
-	public void testClickHome() throws IOException {
-		WebElement profile = webdriver.findElement(By.id("u_0_c"));
-		profile.click();
+    /**
+     * click on message option
+     */
+    @Test(priority = 4)
+    public void testClickMessages() throws IOException {
+        WebElement profile = webdriver.findElement(By.name("mercurymessages"));
+        profile.click();
 
-		webdriver = ScreenShotUtil.takeScreenShot(webdriver);
+        // to take screenshot
+        ScreenShotUtil.takeScreenShot(webdriver);
+    }
 
-	}
+    /**
+     * click on show all messages
+     */
+    @Test(priority = 5)
+    public void testClickMessagesCont() throws IOException {
+        WebElement profile = webdriver.findElement(By.className("_4djt"));
+        profile.click();
 
-	@Test(priority = 4)
-	public void testClickMessages() throws IOException {
-		WebElement profile = webdriver.findElement(By.name("mercurymessages"));
-		profile.click();
+        // to take screenshot
+        ScreenShotUtil.takeScreenShot(webdriver);
 
-		webdriver = ScreenShotUtil.takeScreenShot(webdriver);
-	}
+    }
 
-	@Test(priority = 5)
-	public void testClickMessagesCont() throws IOException {
-		WebElement profile = webdriver.findElement(By.className("_4djt"));
-		profile.click();
+    /**
+     * Logout
+     */
+    @Test(priority = 6)
+    public void logout() throws IOException {
+        webdriver.switchTo().defaultContent();
+        WebElement dropdown = webdriver.findElement(By.id("userNavigationLabel"));
+        dropdown.click();
+        webdriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement signOut = webdriver.findElement(By.partialLinkText("Log Out"));
 
-		webdriver = ScreenShotUtil.takeScreenShot(webdriver);
+        // to take screenshot
+        ScreenShotUtil.takeScreenShot(webdriver);
 
-	}
+        signOut.click();
 
-	/**
-	 * Logout
-	 * 
-	 * @throws InterruptedException
-	 * @throws IOException
-	 */
-	@Test(priority = 6)
-	public void logout() throws IOException {
-		webdriver.switchTo().defaultContent();
-		WebElement dropdown = webdriver.findElement(By.id("userNavigationLabel"));
-		dropdown.click();
-		webdriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		// webdriver.wait(5000);
-		WebElement signOut = webdriver.findElement(By.partialLinkText("Log Out"));
-		signOut.click();
+    }
 
-		webdriver = ScreenShotUtil.takeScreenShot(webdriver);
-	}
-
-	/**
-	 * Closing browser
-	 * 
-	 * @throws IOException
-	 */
-
-	@Test(priority = 7)
-	public void closeBrowser() throws IOException {
-		webdriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		webdriver.close();
-	}
+    /**
+     * Closing browser
+     */
+    @Test(priority = 7)
+    public void closeBrowser() {
+        webdriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webdriver.close();
+    }
 
 }
--- src/test/java/com/testng/ScreenShotUtil.java
+++ src/test/java/com/testng/ScreenShotUtil.java
@@ -1,44 +1,38 @@
 package com.testng;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-
 /**
- * 
  * @author vishal
- *
  */
-public class ScreenShotUtil {
+class ScreenShotUtil {
 
-	static File screenshotFile;
-	static String screenShotName = "test";
+    /**
+     * For taking screenshot this method is used by AppTest class
+     *
+     * @param webdriver webdriver instance is passed
+     */
+    static void takeScreenShot(WebDriver webdriver) throws IOException {
 
-	/**
-	 * For taking screenshot this method is used by AppTest class
-	 * @param webdriver
-	 * @return
-	 * @throws IOException
-	 */
-	public static WebDriver takeScreenShot(WebDriver webdriver) throws IOException {
+        File screenshotFile;
+        String screenShotName = "test_";
 
-		Date date = new Date();
-		DateFormat dateformate = new SimpleDateFormat("dd-mm-yy-hh-mm-ss");
-		String currentdate = dateformate.format(date);
-		screenshotFile = ((TakesScreenshot) webdriver).getScreenshotAs(OutputType.FILE);
-		String imageName = screenShotName + currentdate;
-		String location = System.getProperty("user.dir") + "\\test-output\\screenshot\\" + imageName + ".png";
-		FileUtils.copyFile(screenshotFile, new File(location));
-
-		return webdriver;
-
-	}
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd-mm-yy-hh-mm-ss");
+        String currentDate = dateFormat.format(date);
+        screenshotFile = ((TakesScreenshot) webdriver).getScreenshotAs(OutputType.FILE);
+        String imageName = screenShotName + currentDate;
+        String location = System.getProperty("user.dir") + "\\test-output\\screenshot\\" + imageName + ".png";
+        FileUtils.copyFile(screenshotFile, new File(location));
+    }
 
 }
